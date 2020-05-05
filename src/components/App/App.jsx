900e89a55422e8ebdd37c0967462344d2bf31db1
Andrew2432 <drew.devops@gmail.com>
2020-05-05 20:08:09 +0530

    Made navbar fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import Header from '../Header/Header';
+import Hero from '../Hero/Hero';
 import './App.css';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Hero />
     </Fragment>
   );
 }
