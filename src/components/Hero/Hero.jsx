c331123cfe043f51a0814ce434d123c374bdc33e
Andrew2432 <drew.devops@gmail.com>
2020-05-05 20:17:33 +0530

    Add image to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import hero_image from '../../images/hero.jpg';
 import './Hero.css';
 
 export default function Hero() {
   return (
     <section className={'hero'}>
-      <h2>Hello</h2>
+      <img src={hero_image} alt="Programmer hero for website" />
     </section>
   );
 }
