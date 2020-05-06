67d9102ba499f197ab9f1d51c16784fc5706a261
Andrew2432 <drew.devops@gmail.com>
2020-05-06 09:13:02 +0530

    IMP: Implement responsive sidebar

--- src/components/Header/NavButton/NavButton.jsx
+++ src/components/Header/NavButton/NavButton.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
+import Links from '../Links/Links';
 import './NavButton.css';
 
 export default function NavButton() {
@@ -8,10 +9,19 @@ export default function NavButton() {
     setSideBar(!sideBar);
   }
   return (
-    <Fragment>
+    <div className={'nav-wrap'}>
       <div className={`nav-button`} onClick={toggleSidebar}>
         <span className={`nav-button-line ${sideBar && 'show'}`}></span>
       </div>
-    </Fragment>
+      <div className={`nav-sidebar ${sideBar && 'show'}`}>
+        <div className={`nav-sidebar-inner ${sideBar && 'show'}`}>
+          <div className={`nav-sidebar-inner-inner ${sideBar && 'show'}`}>
+            <ul>
+              <Links />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
